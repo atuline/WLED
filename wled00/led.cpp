479172c18e227dab957fafcce7201fe5ea0a013a
Frank <91616163+softhack007@users.noreply.github.com>
2022-07-22 14:31:53 +0200

    led.cpp: updated comment
    The "global brightness" feature is not SR specific. This code change should only be a temporary solution, until a fix is availeable in upstream WLED.

--- wled00/led.cpp
+++ wled00/led.cpp
@@ -86,7 +86,7 @@ void applyBri() {
   if (!realtimeMode || !arlsForceMaxBri)
   {
     strip.setBrightness(scaledBri(briT));
-    stateChanged = true; //WLEDSR
+    stateChanged = true; //WLEDSR temporary fix, until solved in upstream
   }
 }
 
